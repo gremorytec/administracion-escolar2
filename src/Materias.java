b68a82a301bb0bb8c433aec36ea463198e1d49f8
L22020806@veracruz.tecnm.mx <L22020806@veracruz.tecnm.mx>
2024-09-12 15:33:10 -0600

    Agregue la clase main(Raul y Anthony)

--- src/Materias.java
+++ src/Materias.java
@@ -1,8 +1,8 @@
-public class Materia {
+public class Materias {
     String nombre;
     String codigo;
 
-    Materia(String nombre, String codigo) {
+    Materias(String nombre, String codigo) {
         this.nombre = nombre;
         this.codigo = codigo;
     }
