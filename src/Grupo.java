04f245be062c70a58a9645259118fa7db3b9567f
L22020806@veracruz.tecnm.mx <L22020806@veracruz.tecnm.mx>
2024-09-11 23:50:08 -0600

    Agregue la clase "Grupo"

---
+++ src/Grupo.java
@@ -0,0 +1,9 @@
+public class Grupo {
+    String codigo;
+    Materia materia;
+
+    Grupo(String codigo, Materia materia) {
+        this.codigo = codigo;
+        this.materia = materia;
+    }
+}
