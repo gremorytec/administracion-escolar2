3554490531fb261b0aa600760a93d8b7fb06dc82
AndyGuss <andres.rivera784@gmail.com>
2024-09-11 23:13:00 -0600

    Agregue la clase "Alumno"

---
+++ src/Alumno.java
@@ -0,0 +1,15 @@
+public class Alumno {
+    //Atributos
+    String nombre;
+    String matricula;
+    String especialidad;
+
+    //Construcctores
+
+    Alumno(String nombre, String matrivula, String especialidad) {
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.especialidad = especialidad;
+    }
+
+}
